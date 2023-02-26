c1e4bb82f1f714fb25330a85a0d13774fd8e1540
LannisRaurus <lannisraurus@gmail.com>
2023-02-26 00:18:29 +0000

    Working core functionalities

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 //Math
-#include <chrono>
 #include <math.h>
 #include <iomanip>
 #include <string>
@@ -15,30 +14,71 @@
 #define WIDTH 720
 #define HEIGHT 720
 
+//Initialize SDL Pointers
+TTF_Font* font;
+SDL_Renderer* ren;
+SDL_Window* win;
+SDL_Event ev;
 
-int main(){
-    //Initialize
-    SDL_Init(SDL_INIT_EVERYTHING);
-    TTF_Init();
-    //SDL Variables
-    SDL_Window* win = SDL_CreateWindow("Cronometer - Periodic Intervals",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
-    SDL_Renderer* ren = SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
-    SDL_Event ev;
-    //System variables
-    bool is_open = true;
-    bool start_read = false;
-    int decimal_places = 3;
-    double uncertainty = pow(10,-decimal_places)/2;
-    double *intervals;
-    std::stringstream str_stream;
-    //Textures - Loading
-    TTF_Font* font = TTF_OpenFont("\\munro.ttf",32);
-    if(font==nullptr){std::cerr<<"Error: couldn't load font\n";}
-    std::string title_msg = "Precision is "+std::to_string(decimal_places)+" decimal places.\nSPACE - Start\nESC - End";
-    SDL_Surface* title_surf = TTF_RenderText_Solid(font,"test",{255,255,255});
+//System variables
+bool is_open = true;
+bool start_read = false;
+int decimal_places = 4;
+double uncertainty = pow(10,-decimal_places)/2;
+std::vector<double> time_intervals;
+std::vector<double> times;
+std::stringstream str_stream;
+
+//Make Text Function
+SDL_Texture* make_text(std::string text){
+    SDL_Surface* title_surf = TTF_RenderText_Solid(font,text.c_str(),{255,255,255});
     SDL_Texture* title_text = SDL_CreateTextureFromSurface(ren,title_surf);
     SDL_FreeSurface(title_surf);
-    SDL_Rect title_rect = {0,0,WIDTH,HEIGHT/6};
+    return title_text;
+}
+
+//line class - stores a texture and a rect
+class line{
+    private:
+        SDL_Texture* texture;
+        int size;
+        double offset;
+    public:
+        line(std::string text, double offset){
+            this->offset = offset;
+            this->size = text.size();
+            texture = make_text(text);
+        }
+        int getSize(){return size;}
+        double getOffset(){return offset;}
+        SDL_Texture* getTexture(){return texture;}
+        ~line(){
+            SDL_DestroyTexture(texture);
+        }
+};
+
+//MAIN
+int main(){
+    //Initialize SDL
+    std::cout << "Starting\n";
+    SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+    //SDL Variables - Initialize
+    win = SDL_CreateWindow("Cronometer - Periodic Intervals",SDL_WINDOWPOS_CENTERED,SDL_WINDOWPOS_CENTERED,WIDTH,HEIGHT,SDL_WINDOW_SHOWN);
+    ren = SDL_CreateRenderer(win,-1,SDL_RENDERER_ACCELERATED);
+    //Text
+    font = TTF_OpenFont("slkscr.ttf",22);
+    if(font==nullptr){std::cerr<<"Error: couldn't load font\n";}
+
+    std::string title_msg = "Precision - "+std::to_string(decimal_places)+" digits.";
+    
+    std::vector<line*> lines;
+    lines.push_back(new line(title_msg,0));
+    lines.push_back(new line("SPACE - Start",0));
+    lines.push_back(new line("ESC - End",0));
+    lines.push_back(new line("---------------------------------",0));
+    int default_lines = lines.size();
+
     //App Loop
     while(is_open){
         //Events
@@ -49,32 +89,59 @@ int main(){
                     is_open = false;
                     break;
                 case SDL_KEYDOWN:
-                if(start_read==true){
-                    switch(ev.key.keysym.sym){
-                        default: break;
-                        case SDLK_SPACE:
-                            
-                            break;
-                        case SDLK_ESCAPE:
-
+                switch(ev.key.keysym.sym){
+                    case SDLK_SPACE:
+                        if(start_read==true){
+                            str_stream.clear();
+                            time_intervals.push_back(SDL_GetTicks64());
+                            double interval = (time_intervals[time_intervals.size()-1] - time_intervals[time_intervals.size()-2]);
+                            str_stream << std::setprecision(decimal_places) << interval;
+                            std::string iter;
+                            str_stream >> iter;
+                            iter+=" ms";
+                            lines.push_back(new line(iter,0));
+                            times.push_back(interval);
+                        }else{
+                            time_intervals.clear();
+                            times.clear();
+                            lines.erase(lines.begin()+default_lines,lines.begin()+lines.size());
+                            time_intervals.push_back(SDL_GetTicks64());
+                            lines.push_back(new line("STARTED",0));
+                            start_read = true;
+                        }
+                        break;
+                    case SDLK_ESCAPE:
+                        if(start_read==true){
+                            double average = 0;
+                            for(int i = 0; i < times.size(); i++){
+                                average+=times[i];
+                            }
+                            average/=times.size();
+                            str_stream.clear();
+                            str_stream << std::setprecision(decimal_places) << average;
+                            std::string iter;
+                            str_stream >> iter;
+                            std::string msg = "Average: "+iter;
+                            lines.push_back(new line(msg,0));
                             start_read = false;
-                            break;
-                    }
-                }else{
-                    //Insert clean screen code here
-                    start_read = true;
+                        }
+                        break;
                 }
             }
         }
 
-
+        //Clear Renderer
         SDL_SetRenderDrawColor(ren,0,0,0,255);
         SDL_RenderClear(ren);
 
-        SDL_RenderCopy(ren,title_text,NULL,&title_rect);
-
+        //Render Text lines
+        for(int i = 0; i < lines.size();i++){
+            SDL_Rect line = {lines[i]->getOffset()*WIDTH,i*WIDTH/24,lines[i]->getSize()*3*WIDTH/100,HEIGHT/24};
+            SDL_RenderCopy(ren,lines[i]->getTexture(),NULL,&line);
+        }
+        
+        //Show
         SDL_RenderPresent(ren);
-
     }
     //Clean up
     SDL_DestroyWindow(win);
